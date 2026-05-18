cf0697c1011d55edfed8f60f583d1a2fd42bd48d
Iancu Victor-Iuliu <iancu27victor@gmail.com>
2026-05-18 10:13:55 +0300

    rewrote it... makes more sense

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,22 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [sculptureShown, setSculptureShown] = useState(sculptureList[0]);
+  const [sculptureShown, setSculptureShown] = useState(0);
 
   function nextSculpture() {
-    setSculptureShown(sculptureList[sculptureList.indexOf(sculptureShown) + 1]);
+    setSculptureShown(sculptureShown + 1);
   }
+
+  let currentSculpture = sculptureList[sculptureShown]
 
   return (
     <>
       <button onClick={() => nextSculpture()}></button>
-      <div key={sculptureShown}>
-        <h4>{sculptureShown.name}</h4>
-        <h5>{sculptureShown.artist}</h5>
-        <p>{sculptureShown.description}</p>
-        <img src={sculptureShown.url} alt={sculptureShown.alt} />
+      <div key={currentSculpture}>
+        <h4>{currentSculpture.name}</h4>
+        <h5>{currentSculpture.artist}</h5>
+        <p>{currentSculpture.description}</p>
+        <img src={currentSculpture.url} alt={currentSculpture.alt} />
       </div>
     </>
   );
